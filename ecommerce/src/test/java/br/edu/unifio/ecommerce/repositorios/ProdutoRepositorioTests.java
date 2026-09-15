1d38fd465f4bf90704bc46575480899551a704c9
Sérgio Roberto Delfino <sergio.delfino@fatecourinhos.edu.br>
2026-09-15 23:39:34 +0000

    Aula do Dia 15/09/2026

--- ecommerce/src/test/java/br/edu/unifio/ecommerce/repositorios/ProdutoRepositorioTests.java
+++ ecommerce/src/test/java/br/edu/unifio/ecommerce/repositorios/ProdutoRepositorioTests.java
@@ -1,14 +1,17 @@
 package br.edu.unifio.ecommerce.repositorios;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.math.BigDecimal;
-import java.util.Optional;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
 
 import br.edu.unifio.ecommerce.entidades.Categoria;
 import br.edu.unifio.ecommerce.entidades.Produto;
@@ -35,6 +38,60 @@ public class ProdutoRepositorioTests {
         produtoRepositorio.save(produto);
 
         assertNotNull(produto.getId());
-        assertEquals(2, produto.getId());
+        assertEquals(6, produto.getId());
     }
+
+    @Test 
+    public void deveBuscarUmProdutoPorId () {
+        Produto produto = produtoRepositorio.findById (Integer.parseInt("3")).orElseThrow();
+        
+        assertNotNull(produto);
+        assertEquals("Fone de Ouvido Bluetooth", produto.getNome());
+    }
+
+    @Test 
+    public void deveBuscarTodosOsProdutos () {
+        List<Produto> produtos = produtoRepositorio.findAll(Sort.by("nome"));
+
+        assertEquals(7, produtos.size());
+        assertEquals("Código Limpo", produtos.get(0).getNome());
+        assertEquals("Fone de Ouvido Bluetooth", produtos.get(1).getNome());
+    }
+
+    @Test 
+    public void deveExcluirUmProdutoPorId () {
+        Produto produto = new Produto ();
+        produto.setNome("Nome Teste");
+        produto.setDescricao("Descrição Teste");
+        produto.setPreco(new BigDecimal("1.00"));
+        produto.setEstoque(Short.parseShort("1"));
+
+        Categoria categoria = categoriaRepositorio.findById(Short.parseShort("1")).orElseThrow();
+        produto.setCategoria(categoria);
+
+        produtoRepositorio.save(produto);
+
+        assertTrue(produtoRepositorio.existsById(produto.getId()));
+        produtoRepositorio.deleteById(produto.getId());
+        assertFalse(produtoRepositorio.existsById(produto.getId()));
+    }
+
+    @Test 
+    public void deveAtualizarONomeDeUmProduto () {
+        Produto produto = new Produto ();
+        produto.setNome("Nome Teste");
+        produto.setDescricao("Descrição Teste");
+        produto.setPreco(new BigDecimal("1.00"));
+        produto.setEstoque(Short.parseShort("1"));
+
+        Categoria categoria = categoriaRepositorio.findById(Short.parseShort("1")).orElseThrow();
+        produto.setCategoria(categoria);
+
+        produtoRepositorio.save(produto);
+
+        produto.setNome("Outro Nome Teste"); 
+        produtoRepositorio.save(produto);  
+
+        assertEquals("Outro Nome Teste", produtoRepositorio.findById(produto.getId()).orElseThrow().getNome()); 
+    } 
 }
